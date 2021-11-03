8e9f7187d523609db8a2d72e8e9e098f7791a194
Khaled Naili <k.naili@ewm.swiss>
2021-11-03 19:29:30 +0100

    api

--- src/main/java/com/funsoft/cabinet/controller/controller.java
+++ src/main/java/com/funsoft/cabinet/controller/controller.java
@@ -5,7 +5,10 @@ import com.funsoft.cabinet.Service.Etudiant_serviceImp;
 import com.funsoft.cabinet.model.Etudiant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -16,11 +19,17 @@ public class controller {
 
     @GetMapping("/test")
     public Etudiant test(){
+        for(int i = 0 ; i<10; i++){
+            Etudiant e1 = new Etudiant();
+            e1.setFirstname("khaled".concat(String.valueOf(i)));
+            e1.setLastname("naili".concat(String.valueOf(i)));
+            etudiant_Service.add_entity(e1);
+        }
         Etudiant e1 = new Etudiant();
         e1.setFirstname("khaled");
         e1.setLastname("naili");
-        Etudiant_serviceImp serviceImp = new Etudiant_serviceImp();
         return etudiant_Service.add_entity(e1);
+
     }
     @GetMapping("/users")
     public List<Etudiant> getAllUsers() {
@@ -29,7 +38,16 @@ public class controller {
 
     @GetMapping("/findUser")
     public List<Etudiant> findUser() {
-        return etudiant_Service.find_entity("khaled");
+        return etudiant_Service.find_entity("khaled3");
+    }
+    @RequestMapping(value = "/finduserby",method = RequestMethod.GET)
+    public ModelAndView finduserby() {
+        List<Etudiant> lst = etudiant_Service.findAll();
+        ModelAndView model = new ModelAndView();
+        model.addObject("users",lst);
+        model.setViewName("mes_user");
+        return model;
+
     }
 
 }
